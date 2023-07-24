726fe24913af57e85c82c64364a27117cb185e05
vinicius-airess <vinicius.aires@byxcapital.com.br>
2023-07-24 12:27:23 -0300

    Testes de inserirRegra atualizados

--- cypress/support/gui_commands.js
+++ cypress/support/gui_commands.js
@@ -1,11 +1,8 @@
-Cypress.Commands.add('login', (
-      user = Cypress.env('user_name'),
-      password = Cypress.env('user_password'),
-  ) => {
+Cypress.Commands.add('login', () => {
       cy.visit('/admin/login/')
   
-      cy.get("#id_username").type(user)
-      cy.get("#id_password").type(password, { log: false })
+      cy.get("#id_username").type(Cypress.env('user_name'))
+      cy.get("#id_password").type(Cypress.env('user_password'), { log: false })
       cy.get('.submit-row > input').click()
   })
 
@@ -13,16 +10,27 @@ Cypress.Commands.add('logout', () => {
     cy.get('button').click()
 })
 
-Cypress.Commands.add('inserirRegra', (
-    
-  ) => {
-    cy.get('.model-regra > :nth-child(2) > .addlink').click()
-    cy.get('#id_nome_regra')
-    cy.get('#id_cliente')
-    cy.get('#id_tipo_produto')
-    cy.get('#id_cd_contrato_tipo')
-    cy.get('#id_condicional')
-    cy.get('#id_campo_comparativo')
-    cy.get('.default').click()
-
+Cypress.Commands.add('inserirRegra', () => {
+  Cypress.on('uncaught:exception', (err, runnable) => {
+    return false
   })
+    cy.get('.model-regra > :nth-child(2) > .addlink').click()
+    cy.get('#id_nome_regra').type(Cypress.env('nome_regra'))
+    cy.get('#id_cliente').select(Cypress.env('cliente'))
+    cy.get('#id_tipo_produto').select(Cypress.env('tipo_produto'))
+    cy.get('#id_cd_contrato_tipo').select(Cypress.env('tipo_contrato'))
+    cy.get('#id_condicional').select(Cypress.env('regra'))
+    cy.get('#id_campo_comparativo').type(Cypress.env('valor_comparativo'))
+    cy.get('.default').click()
+})
+
+Cypress.Commands.add('deletarRegra', () => {
+  Cypress.on('uncaught:exception', (err, runnable) => {
+    return false
+  })
+  cy.get('.end').click()
+  cy.get(':last-child > .action-checkbox > .action-select').click()
+  cy.get('select').select(1)
+  cy.get('.button').click()
+  cy.get('div > [type="submit"]').click()
+})
